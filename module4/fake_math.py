f2940856ba9638eac5b1291dd0beb5960f8a0bcc
Алексей Бугаев <aleksey.bugaev@gmail.com>
2024-08-14 22:19:42 +0400

    initial&finish

---
+++ module4/fake_math.py
@@ -0,0 +1,8 @@
+def divide(first, second):
+
+    try:
+        return first / second
+    except ZeroDivisionError:
+        return ("Ошибка!")
+    else:
+        return result
---
+++ module4/true_math.py
@@ -0,0 +1,10 @@
+from math import inf
+
+
+def divide(first, second):
+    try:
+        return first / second
+    except ZeroDivisionError:
+        return inf
+    else:
+        return result
