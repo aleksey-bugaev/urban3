12784bb065df6658f2bbf858bd3bf876f9707425
Алексей Бугаев <aleksey.bugaev@gmail.com>
2024-10-06 17:47:30 +0400

    finish

---
+++ module8/module_8_1.py
@@ -0,0 +1,15 @@
+def add_everything_up(a, b):
+    try:
+        return a + b
+    except TypeError:
+        if isinstance(a, float):
+            a = str(a)
+        if isinstance(b, int):
+            b = str(b)
+
+        return a + b
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
