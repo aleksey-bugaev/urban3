5f5acf86354b2e3ed2b3ca2f0fb70b6afb73ecaa
Алексей Бугаев <aleksey.bugaev@gmail.com>
2024-10-30 22:34:12 +0400

    finish

---
+++ module_9/module_9_7.py
@@ -0,0 +1,28 @@
+def is_prime(func):
+    def wrapper(*args):
+        i = func(*args)
+
+        if i == 1:
+            return "Составное"
+
+        if int(pow(i, 1 / 2)) == 1:
+            return "Простое"
+
+        for n in range(2, int((i ** 0.5)) + 1):
+
+            if i % n == 0:
+                return "Составное"
+            else:
+                return "Простое"
+
+    return wrapper
+
+
+@is_prime
+def sum_three(*args):
+    result = sum(args)
+    return result
+
+
+result = sum_three(2, 3, 6)
+print(result)
